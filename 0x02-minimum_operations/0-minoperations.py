800ac30029c8a6447dafe6d58a518f2d23d44dce
Emmanuel Asante <asantemma7@gmail.com>
2024-06-16 08:06:55 +0000

    Added minimum operations file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A method that calculates the fewest number of operations
+   needed to result in exactly n H characters in the file.
+   given a number n
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# 0x02-minimum_operations
+
+This task devises a solution that efficiently calculates the minimum number of operations to achieve a given number of characters using only “Copy All” and “Paste” operations. 
