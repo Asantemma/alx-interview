6009e707c002d5d9b79f0399d40502abdd4706cf
Emmanuel Asante <asantemma7@gmail.com>
2024-06-23 09:47:05 +0000

    Added log parsing file

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def print_stats(status_counts, total_size):
+    """
+    Print the total file size and the number
+    of occurrences of each status code.
+    Args:
+        status_counts: Dictionary of status codes.
+        total_size: Total file size.
+    """
+    print(f"File size: {total_size}")
+    for status_code in sorted(status_counts.keys()):
+        if status_counts[status_code] > 0:
+            print(f"{status_code}: {status_counts[status_code]}")
+
+
+# Initialize metrics
+total_size = 0
+line_count = 0
+status_counts = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # splits the line into parts
+        parsed_line = parsed_line[::-1]  # reverses the parts for easier access
+
+        if len(parsed_line) > 2:
+            line_count += 1
+
+            total_size += int(parsed_line[0])
+            status_code = parsed_line[1]
+
+            if status_code in status_counts:
+                status_counts[status_code] += 1
+
+            if line_count == 10:
+                print_stats(status_counts, total_size)
+                line_count = 0
+
+finally:
+    print_stats(status_counts, total_size)
