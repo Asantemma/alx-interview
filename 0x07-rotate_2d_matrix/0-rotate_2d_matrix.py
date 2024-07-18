25c91d6b8cdb66078e95967ddd2f08c9d8e71246
Emmanuel Asante <asantemma7@gmail.com>
2024-07-18 23:53:06 +0000

    Added 2D matrix task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,13 +11,13 @@ def rotate_2d_matrix(matrix):
     matrix (list of list of int): The n x n 2D matrix to rotate
     """
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             # Swap element at (i, j) with element at (j, i)
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row
     for i in range(n):
         # Reverse the ith row
